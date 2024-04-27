aafd06e3c689f75c582801c1561459afb28c0c05
Michael <mliggs12@gmail.com>
2024-04-27 12:15:20 -0600

    Update dashboard and add the emotional calibration module

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import EmotionalCalibrationModule from "./components/emotionalCalibrationModule";
 
 export default function Dashboard() {
   return (
-    <main>
-      <h1>Personal Dashboard</h1>
+    <main className="m-2">
+      <h1 className="text-3xl text-center my-4">Personal Dashboard</h1>
+      <div className="flex justify-center">
+        <EmotionalCalibrationModule />
+      </div>
     </main>
   );
 }
