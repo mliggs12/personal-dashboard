19429739ca25a3b42219c05f465901bf2961f1a6
Michael <mliggs12@gmail.com>
2024-06-12 13:20:36 -0600

    Add dark mode and other Shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@
 
 import { SignInButton, UserButton } from "@clerk/nextjs";
 import { Unauthenticated, Authenticated } from "convex/react";
+import NewScheduleForm from "./components/plan/NewScheduleForm";
+import ScheduleList from "./components/plan/ScheduleList";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export default function Dashboard() {
+
   return (
     <main className="m-2">
       <Unauthenticated>
@@ -11,6 +15,9 @@ export default function Dashboard() {
       </Unauthenticated>
       <Authenticated>
         <UserButton />
+        <ModeToggle />
+        {/* <ScheduleList /> */}
+        {/* <NewScheduleForm /> */}
       </Authenticated>
     </main>
   );
