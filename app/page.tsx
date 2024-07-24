c9647e5aff8d34cf050cf89d408313d31e9be2e7
Michael <mliggs12@gmail.com>
2024-07-23 21:58:48 -0600

    Fixed type issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
+import { auth } from "@clerk/nextjs/server";
 import CalendarScheduleView from "@/components/calendar/calendar-schedule-view";
 import AccountDataCard from "@/components/finance/account-data-card";
 
 export default function Dashboard() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return null;
+  }
+
   return (
-    <main className="w-full flex flex-1 flex-col gap-4 p-2 lg:p-4">
+    <main className="w-full flex flex-col flex-1 gap-4 p-2 lg:p-4">
       <div className="flex items-center mb-2">
         <h1 className="text-lg md:text-2xl font-semibold">Dashboard</h1>
       </div>
       <div className="flex gap-4">
         <AccountDataCard />
-        <CalendarScheduleView />
+        <CalendarScheduleView userId={userId} />
       </div>
     </main>
   );
