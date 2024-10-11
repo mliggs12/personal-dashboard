8464176eb052165b972b45556508dbb766f9fa3d
Michael <mliggs12@gmail.com>
2024-10-11 00:26:26 -0600

    Add primary intention to dashboard home

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -19,19 +19,27 @@ export default function DashboardPage() {
   return (
     <div className="container pt-8 pb-8 px-4 sm:px-8">
       <div className="hidden flex-col md:flex">
-        <div className="flex-1 space-y-4 p-8 pt-6">
+        <div className="flex-1 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-3xl 2xl:text-7xl font-bold tracking-tight">
+            <h2 className="text-3xl md:text-7xl font-bold tracking-tight">
               Dashboard
             </h2>
             <div className="flex items-center justify-center">
               <p className="text-lg">{formatTimestamp(Date.now())}</p>
             </div>
           </div>
+          <div className="border-y-2 p-4 space-y-2 my-12">
+            <h3 className="text-3xl font-semibold">Intention for my life</h3>
+            <p className="text-2xl font-light italic">
+              Alignment with the true self; To live with a consistent and
+              unconditional feeling of a fullness of being with an inner
+              satisfaction and a passion for life and for living.
+            </p>
+          </div>
           <Button
             asChild
             size="sm"
-            className="ml-auto gap-1"
+            className="ml-auto gap-1 mb-4"
           >
             <Link href="/me5">
               Mind Dump
