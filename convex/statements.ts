d7d0830696ca70122980dea569192d0062a2276a
Michael <mliggs12@gmail.com>
2024-10-14 23:09:28 -0600

    Refactor statements.ts

--- convex/statements.ts
+++ convex/statements.ts
@@ -26,54 +26,6 @@ export const createMindDumpStatement = mutation({
   },
 });
 
-export const todayMindDumpStatements = query({
-  args: {},
-  async handler(ctx) {
-    const now = new Date();
-    const startOfDay = new Date(
-      Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), 3, 0, 0),
-    );
-    const endOfDay = new Date(
-      Date.UTC(now.getFullYear(), now.getMonth(), now.getDate() + 1, 3, 0, 0),
-    );
-
-    return (
-      (await ctx.db
-        .query("statements")
-        .withIndex("by_type", (q) => q.eq("type", "mind_dump"))
-        .filter(
-          (q) =>
-            q.gte(q.field("date"), startOfDay.toISOString()) &&
-            q.lt(q.field("date"), endOfDay.toISOString()),
-        )
-        .collect()) || []
-    );
-  },
-});
-
-// // New mutation to update the status of a statement
-// export const updateStatus = mutation({
-//   args: {
-//     statementId: v.id("statements"),
-//     status: v.union(v.literal("active"), v.literal("done")),
-//   },
-//   async handler(ctx, { statementId, status }) {
-//     await ctx.db.patch(statementId, { status });
-//   },
-// });
-
-// // New mutation to update the description of a statement
-// export const updateDescription = mutation({
-//   args: {
-//     statementId: v.id("statements"),
-//     description: v.string(),
-//   },
-//   async handler(ctx, { statementId, description }) {
-//     await ctx.db.patch(statementId, { description });
-//   },
-// });
-
-// New query to get a single statement by ID
 export const get = query({
   args: { statementId: v.id("statements") },
   async handler(ctx, { statementId }) {
