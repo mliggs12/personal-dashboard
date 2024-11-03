255c05e3e5111efdc53b872bcc246d185d7f0416
Michael <mliggs12@gmail.com>
2024-11-03 15:47:31 -0700

    Add convex

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 const authConfig = {
   providers: [
     {
-      domain: "https://clerk.enthousiazein.me",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ],
