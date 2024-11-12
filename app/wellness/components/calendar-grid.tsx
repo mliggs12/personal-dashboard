2f169df031452ee9e40659dce074bd7fa04e7161
Michael <mliggs12@gmail.com>
2024-11-12 15:56:15 -0700

    Add Stoic Life Calendar for fun

---
+++ app/wellness/components/calendar-grid.tsx
@@ -0,0 +1,20 @@
+import dayjs from "dayjs";
+import isoWeek from "dayjs/plugin/isoWeek";
+import { weeksAliveSinceBirth } from "./utils.stoic";
+import WeekCheckbox from "./week-checkbox";
+
+dayjs.extend(isoWeek);
+
+export default function CalendarGrid({ birthday }: { birthday: string }) {
+  return (
+    <div className="grid grid-cols-[repeat(52,minmax(0,1fr))] grid-rows-[repeat(88,minmax(0,1fr))] gap-2">
+      {Array.from({ length: 88 * 52 }).map((_, index) => (
+        <WeekCheckbox
+          key={index}
+          index={index}
+          weeksAlive={weeksAliveSinceBirth(birthday)}
+        />
+      ))}
+    </div>
+  );
+}
