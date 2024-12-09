3bd4b930b988df306e7e1b824b7911fac6abbd56
Michael <mliggs12@gmail.com>
2024-12-09 15:30:38 -0700

    Remove redundant auth check for dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,21 +1,10 @@
-"use client";
-
-import { useCurrentUser } from "@/hooks/useCurrentUser";
 import TasksCard from "./components/tasks/tasks-card";
 
 export default function DashboardPage() {
-  const { isLoading, isAuthenticated } = useCurrentUser();
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isAuthenticated) {
-    return (
-      <div className="h-full flex justify-center">
-        <TasksCard />
-        {/* <CalendarScheduleView /> */}
-      </div>
-    );
-  }
+  return (
+    <div className="h-full flex justify-center">
+      <TasksCard />
+      {/* <CalendarScheduleView /> */}
+    </div>
+  );
 }
