911d63d730a283f6404648723fa1af6bf2a7c12f
Michael <mliggs12@gmail.com>
2024-12-31 19:26:43 -0700

    remove incomplete changes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,10 @@
-import { Id } from "@/convex/_generated/dataModel";
-
-import ChatBox from "./components/messenger/chat-box";
 import TasksCard from "./components/tasks/tasks-card";
 
 export default function DashboardPage() {
-  const staticChatId = "pn7555asfrcprprgr6cc1rdg9x76aqdr" as Id<"chats">;
-
   return (
     <div className="h-full flex flex-col flex-1">
       <TasksCard />
       {/* <CalendarScheduleView /> */}
-      {/* <ChatBox chatId={staticChatId} /> */}
     </div>
   );
 }
