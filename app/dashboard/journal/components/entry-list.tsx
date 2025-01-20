87073d8054740db696b197badf86ee3cf570c1a5
Michael <mliggs12@gmail.com>
2025-01-19 17:21:49 -0700

    change journal entry list to 2 column format

--- app/dashboard/journal/components/entry-list.tsx
+++ app/dashboard/journal/components/entry-list.tsx
@@ -9,15 +9,19 @@ export default function EntryList() {
   const entries = useQuery(api.journalEntries.list);
 
   return (
-    <div>
+    <div className="grid grid-cols-[1fr,auto] gap-x-4">
       {entries?.map((entry) => (
-        <div
-          key={entry._id}
-          className="flex justify-between text-base"
-        >
-          {entry.content}
-          <span>{timestampToTime(entry._creationTime)}</span>
-        </div>
+        <>
+          <div
+            key={entry._id}
+            className="text-base break-words"
+          >
+            {entry.content}
+          </div>
+          <div className="text-base text-muted-foreground whitespace-nowrap">
+            {timestampToTime(entry._creationTime)}
+          </div>
+        </>
       ))}
     </div>
   );
