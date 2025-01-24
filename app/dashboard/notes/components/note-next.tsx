e56a0794c4c1115b92f9eee6075db90222c1616e
Michael <mliggs12@gmail.com>
2025-01-24 11:19:36 -0700

    note refactor

--- app/dashboard/notes/components/note-next.tsx
+++ app/dashboard/notes/components/note-next.tsx
@@ -7,25 +7,25 @@ import { Textarea } from "@/components/ui/textarea";
 import { api } from "@/convex/_generated/api";
 import { Doc, Id } from "@/convex/_generated/dataModel";
 
-export default function NoteContent({ note }: { note: Doc<"notes"> }) {
-  const [content, setContent] = useState(note.content);
+export default function NoteText({ note }: { note: Doc<"notes"> }) {
+  const [text, setContent] = useState(note.text);
   const updateNote = useMutation(api.notes.update);
 
   useEffect(() => {
     const saveInterval = setInterval(() => {
-      if (content !== note.content) {
-        updateNote({ noteId: note._id as Id<"notes">, content: content! });
+      if (text !== note.text) {
+        updateNote({ noteId: note._id as Id<"notes">, text: text! });
       }
     }, 1000);
 
     return () => clearInterval(saveInterval);
-  }, [content, note.content, note._id, updateNote]);
+  }, [text, note.text, note._id, updateNote]);
 
   return (
     <Textarea
       onChange={(e) => setContent(e.target.value)}
       placeholder="Note"
-      value={content}
+      value={text}
       className="flex flex-col flex-1 h-full p-0 border-none focus-visible:ring-0 focus-visible:ring-offset-0"
     />
   );
