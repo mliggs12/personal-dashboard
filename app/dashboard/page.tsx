ecc4491b73a80a4e0adf7c4921a0b11d099984e6
Michael <mliggs12@gmail.com>
2025-02-03 17:14:54 -0700

    Fix auth error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { redirect } from "next/navigation";
+
+import { useCurrentUser } from "@/hooks/useCurrentUser";
+
 import CalendarScheduleView from "./components/calendar/calendar-schedule-view";
 import TasksCard from "./components/tasks/tasks-card";
 
 export default function DashboardPage() {
+  const { isLoading, isAuthenticated } = useCurrentUser();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (!isAuthenticated) {
+    redirect("/");
+  };
+
   return (
     <div className="h-full flex flex-col md:flex-row flex-1 p-4 gap-4">
       <TasksCard />
