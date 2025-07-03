b4e18ed75464d5c6d3ad740b895c3f09587871fb
Michael <mliggs12@gmail.com>
2025-07-02 18:55:47 -0600

    Upgrade to Next.js to version 15

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  images: {
+    remotePatterns: [new URL('https://nix-tag-images.s3.amazonaws.com/**')]
+  }
+};
 
 export default nextConfig;
